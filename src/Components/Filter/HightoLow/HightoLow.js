5cf53c4c6bbbd9f1f579e9899c8d59b85b7d987f
Kagruma <nguyene375@gmail.com>
2021-02-24 17:29:58 -0500

    added build folder

--- src/Components/Filter/HightoLow/HightoLow.js
+++ src/Components/Filter/HightoLow/HightoLow.js
@@ -3,10 +3,10 @@ const HightoLow = (props) => {
 
   props.Data.sort(function (a, b) { //take a function to compare two things
     if (parseInt(a.price) < parseInt(b.price)) {
-      return 1
+      return 1;
     }
     if (parseInt(a.price) > parseInt(b.price)) {
-      return -1
+      return -1;
     }
   })
 }
